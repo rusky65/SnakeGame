0fa20fff270975f34f0571ad6fa81ad231785805
rusky65 <->
2018-06-23 18:30:11 +0200

    #2: Create pendulum to get the snake stepping forward.

--- SnakeGame/SnakeGame/Model/Arena.cs
+++ SnakeGame/SnakeGame/Model/Arena.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace SnakeGame.Model {
     /// <summary>
     /// It contains the logic of game
     /// </summary>
     class Arena {
+        private bool isStarted;
+        private DispatcherTimer pendulum;
         private Snake snake;
         private MainWindow View;
 
@@ -19,9 +23,49 @@ namespace SnakeGame.Model {
 
             snake = new Snake(10, 10);
 
-            var cell = View.ArenaGrid.Children[10 * 20 + 10];
+            pendulum = new DispatcherTimer(TimeSpan.FromMilliseconds(500),DispatcherPriority.Normal, ItsTimeToDisplay, Application.Current.Dispatcher);
+
+            isStarted = false;
+
+        }
+
+        private void ItsTimeToDisplay(object sender, EventArgs e) {
+
+            if (!isStarted) {
+                return;
+            }
+
+            var currentPosition = new ArenaPosition(snake.HeadPosition.RowPosition, snake.HeadPosition.ColumnPosition);
+
+            // clac the head next position
+            switch (snake.HeadDirection) {
+                case SnakeHeadDirectionEnum.Up:
+                    snake.HeadPosition.RowPosition = snake.HeadPosition.RowPosition - 1;
+                    break;
+                case SnakeHeadDirectionEnum.Down:
+                    snake.HeadPosition.RowPosition = snake.HeadPosition.RowPosition + 1;
+                    break;
+                case SnakeHeadDirectionEnum.Left:
+                    snake.HeadPosition.ColumnPosition = snake.HeadPosition.ColumnPosition - 1;
+                    break;
+                case SnakeHeadDirectionEnum.Right:
+                    snake.HeadPosition.ColumnPosition = snake.HeadPosition.ColumnPosition + 1;
+                    break;
+                case SnakeHeadDirectionEnum.InPlace:
+                    break;
+                default:
+                    break;
+            }
+
+            // show the new position
+            var cell = View.ArenaGrid.Children[snake.HeadPosition.RowPosition * 20 + snake.HeadPosition.ColumnPosition];
             var image = (FontAwesome.WPF.ImageAwesome)cell;
             image.Icon = FontAwesome.WPF.FontAwesomeIcon.Circle;
+
+            // hide the current position
+            cell = View.ArenaGrid.Children[currentPosition.RowPosition * 20 + currentPosition.ColumnPosition];
+            image = (FontAwesome.WPF.ImageAwesome)cell;
+            image.Icon = FontAwesome.WPF.FontAwesomeIcon.SquareOutline;
         }
 
         internal void KeyDown(KeyEventArgs e) {
@@ -30,12 +74,34 @@ namespace SnakeGame.Model {
                 case Key.Up:
                 case Key.Right:
                 case Key.Down:
-                    View.GamePlayTextBlockBorder.Visibility = System.Windows.Visibility.Hidden;
-                    View.NumberOfMealsTextBlock.Visibility = System.Windows.Visibility.Visible;
-                    View.ArenaGrid.Visibility = System.Windows.Visibility.Visible;
+                    if (!isStarted) {
+                        StartNewGame();
+                    }
+
+                    switch (e.Key) {
+                        case Key.Left:
+                            snake.HeadDirection = SnakeHeadDirectionEnum.Left;
+                            break;
+                        case Key.Up:
+                            snake.HeadDirection = SnakeHeadDirectionEnum.Up;
+                            break;
+                        case Key.Right:
+                            snake.HeadDirection = SnakeHeadDirectionEnum.Right;
+                            break;
+                        case Key.Down:
+                            snake.HeadDirection = SnakeHeadDirectionEnum.Down;
+                            break;
+                    }
                     Console.WriteLine(e.Key);
                     break;
             }
+        }
+
+        private void StartNewGame() {
+            View.GamePlayTextBlockBorder.Visibility = System.Windows.Visibility.Hidden;
+            View.NumberOfMealsTextBlock.Visibility = System.Windows.Visibility.Visible;
+            View.ArenaGrid.Visibility = System.Windows.Visibility.Visible;
+            isStarted = true;
         }
     }
 }
